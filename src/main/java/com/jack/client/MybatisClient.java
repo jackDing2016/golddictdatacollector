c805ce707ff6e96ac1bc85ac7c5da6fdb7ba70cd
jack <jackding2015@gmail.com>
2023-06-07 07:17:39 -0400

    commit

--- src/main/java/com/jack/client/MybatisClient.java
+++ src/main/java/com/jack/client/MybatisClient.java
@@ -3,6 +3,8 @@ package com.jack.client;
 import com.jack.dao.DefinitionMapper;
 import com.jack.dao.WordMapper;
 import com.mysql.cj.jdbc.MysqlDataSource;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,16 +12,31 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
-import javax.sql.DataSource;
+import java.io.File;
 
 public class MybatisClient {
 
     public static SqlSessionFactory getSqlSessionFactory() {
+
+        // read datasource info from properties file
+        Configurations configurations = new Configurations();
+        org.apache.commons.configuration2.Configuration dbConfiguration = null;
+        try {
+            dbConfiguration = configurations.properties(new File("database.properties"));
+        } catch (ConfigurationException e) {
+            throw new RuntimeException();
+        }
+
+        String url = dbConfiguration.getString("database.url");
+        String user = dbConfiguration.getString("database.user");
+        String password = dbConfiguration.getString("database.password");
+
+
         // datasource
         MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/goldendict");
-        dataSource.setUser("root");
-        dataSource.setPassword("880201");
+        dataSource.setUrl(url);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
 
         // transaction factory
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
@@ -27,16 +44,16 @@ public class MybatisClient {
         // environment
         Environment environment = new Environment("development", transactionFactory, dataSource);
 
-        // configuration
+        // dbConfiguration
         Configuration configuration = new Configuration(environment);
         configuration.addMapper(WordMapper.class);
         configuration.addMapper(DefinitionMapper.class);
 
 
-//        configuration.addMappers("com.jack.xml");
-//        configuration.resour
-//        configuration.addMappedStatement();
-//        configuration.add
+//        dbConfiguration.addMappers("com.jack.xml");
+//        dbConfiguration.resour
+//        dbConfiguration.addMappedStatement();
+//        dbConfiguration.add
         // sql session factory
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
 

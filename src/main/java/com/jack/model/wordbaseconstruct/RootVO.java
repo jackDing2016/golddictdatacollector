d0dc2b91df12e7cbf6219912dfc1678393fdbd56
jack <jackding2015@gmail.com>
2023-06-18 03:37:21 -0400

    Root VO add origin member

--- src/main/java/com/jack/model/wordbaseconstruct/RootVO.java
+++ src/main/java/com/jack/model/wordbaseconstruct/RootVO.java
@@ -3,7 +3,18 @@ package com.jack.model.wordbaseconstruct;
 import com.jack.model.vo.WordVO;
 
 public class RootVO extends WordVO {
+
+    private String origin;
+
     public RootVO() {
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
     }
 
     public RootVO(String name, String definition) {
